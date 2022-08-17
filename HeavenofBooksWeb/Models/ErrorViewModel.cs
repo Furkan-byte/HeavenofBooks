b7017ce28a69ae7f7e2646f64cacab0fae1c630d
Furkan-byte <56610496+Furkan-byte@users.noreply.github.com>
2022-08-18 00:04:02 +0300

    carrying

---
+++ HeavenofBooksWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HeavenofBooksWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
