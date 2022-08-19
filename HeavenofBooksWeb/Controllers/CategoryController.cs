8585a9f80496db690bdcbb512d7ae1236dff9a33
Furkan-byte <56610496+Furkan-byte@users.noreply.github.com>
2022-08-19 15:51:54 +0300

    day2 update
     migration added - database created and some pages html css codes

---
+++ HeavenofBooksWeb/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using HeavenofBooksWeb.Data;
+using HeavenofBooksWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeavenofBooksWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> dbContextVariables = _db.Categories;
+            return View(dbContextVariables);
+        }
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateCategory(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name and DisplayOrder values are matching!!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+    }
+}
